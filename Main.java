528fa3489312a24aa68a1964f0aa08838132e5f6
KungfuKenny1908 <leonardosilvalls1908@gmail.com>
2025-03-14 18:36:45 -0300

    exercicio f

--- Main.java
+++ Main.java
@@ -5,15 +5,18 @@ public class Main {
     public static void main(String[] args) {
         
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(10);
-        list.add(10);
-        list.add(10);
-        list.add(10);
 
-        //System.out.println(list);
-        //System.out.println(hasRepeat(list));
-        System.out.println(nroRepeat(list));    
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
 
+        ArrayList<Integer> list2 = new ArrayList<>();
+
+        for(int i = 1; i < 10; i++) {
+            list2.add(i);
+        }
+
+        System.out.println(intersection(list, list2));
     }
 
     private static int nOcorrencias(ArrayList<Integer> l, Integer j) {
@@ -38,18 +41,19 @@ public class Main {
         return false;
     }
 
-    private static int nroRepeat(ArrayList<Integer> l) {
-        int repeatedNums = 0;
-        for (int i = 0; i < l.size(); i++) {
-            int current = l.get(i); 
-            for (int j = i + 1; j < l.size(); j++) {
-                if (l.get(j)==current) {
-                    repeatedNums+=1;
-                    continue;
+    private static ArrayList<Integer> intersection(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+        
+        ArrayList<Integer> intersecNums = new ArrayList<>();
+
+        for (int i = 0; i < l1.size(); i++) {
+            int current = l1.get(i);
+            for (int j = 0; j < l2.size(); j++){
+                if (current == l2.get(j)) {
+                    intersecNums.add(current);
                 }
             }
         }
-        return repeatedNums;
+        return intersecNums;
     }
 
 
