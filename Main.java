44dfc5fde862693c4a96736d27875cd0c565a463
pgchiella <chiellahimself@gmail.com>
2025-03-14 18:52:55 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -58,3 +58,129 @@ public class Main {
 
 
 }
+
+public class ElementosRepetidos
+{
+    public static int nroRepetir(ArrayList<Integer> l)
+    {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        int count = 0;
+
+        for (int num : l) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (int freq : freqMap.values()) {
+            if (freq > 1) {
+                count += freq - 1;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 2, 3, 4, 4, 4, 5));
+        System.out.println(nroRepetir(list));
+    }
+    }
+
+public class RepeatedElements
+{
+    public static int nroRepeat(ArrayList<Integer> l)
+    {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        int count = 0;
+
+        for (int num : l) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (int freq : freqMap.values())
+        {
+            if (freq > 1)
+            {
+                count += freq - 1; // Contabiliza os elementos repetidos
+            }
+        }
+
+        return count;
+    }
+
+    public static ArrayList<Integer> listRepeat(ArrayList<Integer> l) {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        ArrayList<Integer> repeatedElements = new ArrayList<>();
+
+        for (int num : l) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            if (entry.getValue() > 1) {
+                repeatedElements.add(entry.getKey());
+            }
+        }
+
+        return repeatedElements;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 2, 3, 4, 4, 4, 5));
+        System.out.println(nroRepeat(list));
+        System.out.println(listRepeat(list)); 
+    }
+}
+
+public class RepeatedElements {
+    public static int nroRepeat(ArrayList<Integer> l) {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        int count = 0;
+
+        for (int num : l) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (int freq : freqMap.values()) {
+            if (freq > 1) {
+                count += freq - 1; // Contabiliza os elementos repetidos
+            }
+        }
+        
+        return count;
+    }
+
+    public static ArrayList<Integer> listRepeat(ArrayList<Integer> l) {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        ArrayList<Integer> repeatedElements = new ArrayList<>();
+
+        for (int num : l) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            if (entry.getValue() > 1) {
+                repeatedElements.add(entry.getKey());
+            }
+        }
+
+        return repeatedElements;
+    }
+
+    public static ArrayList<Integer> union(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+        HashSet<Integer> unionSet = new HashSet<>(l1);
+        unionSet.addAll(l2);
+        return new ArrayList<>(unionSet);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 2, 3, 4, 4, 4, 5));
+        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(3, 5, 6, 7));
+        
+        System.out.println(nroRepeat(list)); // Saída esperada: 3
+        System.out.println(listRepeat(list)); // Saída esperada: [2, 4]
+        System.out.println(union(list, list2)); // Saída esperada: [1, 2, 3, 4, 5, 6, 7]
+    }
+}
+
+
